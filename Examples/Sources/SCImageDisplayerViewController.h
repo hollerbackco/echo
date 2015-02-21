22fdcaad0b9a9a54a5bee828baaba502bf827365
Will Dennis <williamldennis@gmail.com>
2015-02-21 15:42:19 -0500

    have basic parse uploading with timestamp for 'year from now'

--- Examples/Sources/SCImageDisplayerViewController.h
+++ Examples/Sources/SCImageDisplayerViewController.h
@@ -14,4 +14,13 @@
 @property (nonatomic, strong) UIImage *photo;
 @property (weak, nonatomic) IBOutlet SCSwipeableFilterView *filterSwitcherView;
 
+//timer buttons
+- (IBAction)oneYearButton:(UIButton *)sender;
+- (IBAction)oneMonthButton:(UIButton *)sender;
+- (IBAction)oneWeekButton:(UIButton *)sender;
+- (IBAction)oneDayButton:(UIButton *)sender;
+- (IBAction)oneHourButton:(UIButton *)sender;
+- (IBAction)customTimeButton:(UIButton *)sender;
+- (IBAction)surpriseMeButton:(UIButton *)sender;
+
 @end
--- Examples/Sources/SCImageDisplayerViewController.m
+++ Examples/Sources/SCImageDisplayerViewController.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2013年 rFlex. All rights reserved.
 //
 
+#import <Parse/Parse.h>
 #import "SCImageDisplayerViewController.h"
 
 @interface SCImageDisplayerViewController () {
@@ -76,4 +77,89 @@
     UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
 }
 
+
+// COME BACK BUTTONS
+
+- (IBAction)oneYearButton:(UIButton *)sender {
+    
+    //sc recorder code
+    UIImage *image = [self.filterSwitcherView currentlyDisplayedImageWithScale:self.photo.scale orientation:self.photo.imageOrientation];
+
+    //date adding math
+    NSDate *today = [[NSDate alloc] init];
+    NSCalendar *gregorian = [[NSCalendar alloc]
+                             initWithCalendarIdentifier:NSGregorianCalendar];
+    NSDateComponents *offsetComponents = [[NSDateComponents alloc] init];
+    [offsetComponents setHour:8760];
+    //    [offsetComponents setMinute:30];
+    NSDate *aYearFromNow = [gregorian dateByAddingComponents:offsetComponents
+                                                       toDate:today options:0];
+    
+    //converting image file
+//    NSData *imageData = UIImagePNGRepresentation(image);
+    NSData *imageData = UIImageJPEGRepresentation(image, 0.9f);
+    PFFile *imageFile = [PFFile fileWithName:@"comebackimage.png" data:imageData];
+    
+    
+    //uploading PFObject
+    PFObject *timerImage = [PFObject objectWithClassName:@"TimerImage"];
+    timerImage[@"image"] = imageFile;
+    timerImage[@"comebacktime"] = aYearFromNow;
+    timerImage[@"delayType"] = @"One Year";
+    [timerImage saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (succeeded) {
+            // The object has been saved.
+        } else {
+            // There was a problem, check error.description
+        }
+    }];
+    
+    //finished saving
+    UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+    
+}
+
+- (IBAction)oneMonthButton:(UIButton *)sender {
+}
+
+- (IBAction)oneWeekButton:(UIButton *)sender {
+}
+
+- (IBAction)oneDayButton:(UIButton *)sender {
+}
+
+- (IBAction)oneHourButton:(UIButton *)sender {
+    
+    //sc recorder code
+    UIImage *image = [self.filterSwitcherView currentlyDisplayedImageWithScale:self.photo.scale orientation:self.photo.imageOrientation];
+    
+    UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+    
+    //date adding math
+    NSDate *today = [[NSDate alloc] init];
+    NSCalendar *gregorian = [[NSCalendar alloc]
+                             initWithCalendarIdentifier:NSGregorianCalendar];
+    NSDateComponents *offsetComponents = [[NSDateComponents alloc] init];
+    [offsetComponents setHour:1];
+//    [offsetComponents setMinute:30];
+    NSDate *anHourFromNow = [gregorian dateByAddingComponents:offsetComponents
+                                                       toDate:today options:0];
+    
+    PFObject *timerImage = [PFObject objectWithClassName:@"TimerImage"];
+    timerImage[@"comebacktime"] = anHourFromNow;
+    timerImage[@"delayType"] = @"One Hour";
+    [timerImage saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (succeeded) {
+            // The object has been saved.
+        } else {
+            // There was a problem, check error.description
+        }
+    }];
+}
+
+- (IBAction)customTimeButton:(UIButton *)sender {
+}
+
+- (IBAction)surpriseMeButton:(UIButton *)sender {
+}
 @end
