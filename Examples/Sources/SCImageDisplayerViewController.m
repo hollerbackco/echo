60f12a912a3aedaafb135c23a4e187f40fb0240c
Will Dennis <williamldennis@gmail.com>
2015-02-21 16:06:43 -0500

    have year from now set up with image, parse, upload, decent alert flow, and customized alert

--- Examples/Sources/SCImageDisplayerViewController.m
+++ Examples/Sources/SCImageDisplayerViewController.m
@@ -62,11 +62,63 @@
 
 }
 
+//ALERTS
+
+//camera save alert
 - (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo {
     if (error == nil) {
         [[[UIAlertView alloc] initWithTitle:@"Done!" message:@"" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
     } else {
         [[[UIAlertView alloc] initWithTitle:@"Failed :(" message:error.localizedDescription delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+    }
+    
+}
+
+//year alert
+- (void)image:(UIImage *)image didFinishSavingYearWithError:(NSError *)error contextInfo:(void *)contextInfo {
+    if (error == nil) {
+        // Create Alert and set the delegate to listen events
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Nice!"
+                                                        message:@"We'll return this in a year from now :)"
+                                                       delegate:self
+                                              cancelButtonTitle:nil
+                                              otherButtonTitles:@"OK", nil];
+        // Set the tag to alert unique among the other alerts.
+        // So that you can find out later, which alert we are handling
+        alert.tag = 100;
+        [alert show];
+        
+    } else {
+        [[[UIAlertView alloc] initWithTitle:@"Failed :(" message:error.localizedDescription delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+    }
+    
+}
+
+//ALERT BUTTON PRESS ACTION - CAN BE SAME FOR ALL ALERTS FROM THE TIME BUTTONS - SEND BACK TO CAMERA
+
+-(void) alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
+    
+    // Is this my Alert View?
+    if (alertView.tag == 100) {
+        //Yes
+        
+        // You need to compare 'buttonIndex' & 0 to other value(1,2,3) if u have more buttons.
+        // Then u can check which button was pressed.
+        if (buttonIndex == 0) {// 1st Other Button
+            
+            //go back to camera
+            [self.navigationController popViewControllerAnimated:YES];
+            
+        }
+        else if (buttonIndex == 1) {// 2nd Other Button
+            
+        }
+        
+    }
+    else {
+        //No
+        // Other Alert View
+        
     }
     
 }
@@ -78,7 +130,7 @@
 }
 
 
-// COME BACK BUTTONS
+// TIME FROM NOW BUTTONS
 
 - (IBAction)oneYearButton:(UIButton *)sender {
     
@@ -90,13 +142,14 @@
     NSCalendar *gregorian = [[NSCalendar alloc]
                              initWithCalendarIdentifier:NSGregorianCalendar];
     NSDateComponents *offsetComponents = [[NSDateComponents alloc] init];
+    
+    //WHERE YOU SET THE TIME FROM NOW IN HOURSE
     [offsetComponents setHour:8760];
     //    [offsetComponents setMinute:30];
     NSDate *aYearFromNow = [gregorian dateByAddingComponents:offsetComponents
                                                        toDate:today options:0];
     
     //converting image file
-//    NSData *imageData = UIImagePNGRepresentation(image);
     NSData *imageData = UIImageJPEGRepresentation(image, 0.9f);
     PFFile *imageFile = [PFFile fileWithName:@"comebackimage.png" data:imageData];
     
@@ -114,8 +167,10 @@
         }
     }];
     
-    //finished saving
-    UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+    //finished saving trigger alert
+    UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingYearWithError:contextInfo:), nil);
+    
+
     
 }
 
@@ -130,31 +185,6 @@
 
 - (IBAction)oneHourButton:(UIButton *)sender {
     
-    //sc recorder code
-    UIImage *image = [self.filterSwitcherView currentlyDisplayedImageWithScale:self.photo.scale orientation:self.photo.imageOrientation];
-    
-    UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
-    
-    //date adding math
-    NSDate *today = [[NSDate alloc] init];
-    NSCalendar *gregorian = [[NSCalendar alloc]
-                             initWithCalendarIdentifier:NSGregorianCalendar];
-    NSDateComponents *offsetComponents = [[NSDateComponents alloc] init];
-    [offsetComponents setHour:1];
-//    [offsetComponents setMinute:30];
-    NSDate *anHourFromNow = [gregorian dateByAddingComponents:offsetComponents
-                                                       toDate:today options:0];
-    
-    PFObject *timerImage = [PFObject objectWithClassName:@"TimerImage"];
-    timerImage[@"comebacktime"] = anHourFromNow;
-    timerImage[@"delayType"] = @"One Hour";
-    [timerImage saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
-        if (succeeded) {
-            // The object has been saved.
-        } else {
-            // There was a problem, check error.description
-        }
-    }];
 }
 
 - (IBAction)customTimeButton:(UIButton *)sender {
