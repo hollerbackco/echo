52da04648e050e4b490eaac4b46cf49a9e8def7b
Will Dennis <williamldennis@gmail.com>
2015-03-02 01:03:49 -0500

    about to start add friends and sign up

---
+++ Examples/Sources/AddFriendsViewController.h
@@ -0,0 +1,15 @@
+//
+//  AddFriendsViewController.h
+//  SCRecorderExamples
+//
+//  Created by Will Dennis on 3/2/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import <ParseUI/ParseUI.h>
+#import <Parse/Parse.h>
+
+@interface AddFriendsViewController : UIViewController
+
+@end
---
+++ Examples/Sources/AddFriendsViewController.m
@@ -0,0 +1,49 @@
+//
+//  AddFriendsViewController.m
+//  SCRecorderExamples
+//
+//  Created by Will Dennis on 3/2/15.
+//
+//
+
+#import "AddFriendsViewController.h"
+
+
+@interface AddFriendsViewController ()
+
+@end
+
+@implementation AddFriendsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+- (void)signUp {
+    PFUser *user = [PFUser user];
+    user.username = @"my name";
+    user.password = @"my pass";
+    user.email = @"email@example.com";
+    
+    // other fields can be set just like with PFObject
+    user[@"phone"] = @"415-392-0202";
+    
+    [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (!error) {
+            // Hooray! Let them use the app now.
+        } else {
+            NSString *errorString = [error userInfo][@"error"];
+            // Show the errorString somewhere and let the user try again.
+        }
+    }];
+}
+
+@end
